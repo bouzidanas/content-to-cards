d9c21c74d7bde6d38061d2eb50d1ebf5765ceb94
bouzidanas <anasbouzid@gmail.com>
2024-03-14 10:52:06 -0500

    add slide navigation bars to slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RevealSlides, RevealHandle } from 'react-reveal-slides'
+import ReveaMarkdown from 'reveal.js/plugin/markdown/markdown';
+import { PiCaretCircleRightLight, PiCaretCircleLeftLight } from "react-icons/pi";
+import Reveal from "reveal.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+import { useRef, useState } from 'react';
+
+function Cards({markdown, cardTitles}:{markdown: string, cardTitles: string[]}) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slidesRef = useRef<RevealHandle>(null);
+
+  const handleOnStateChange = (state: Reveal.RevealState) => {
+    setCurrentSlide(state.indexh);
+  }
+
+  const nextSlide = () => {
+    if (slidesRef.current?.getReveal()?.isLastSlide() === false) {
+      slidesRef.current?.getReveal()?.next();
+    }
+  }
+
+  const prevSlide = () => {
+    if (slidesRef.current?.getReveal()?.isFirstSlide() === false) {
+      slidesRef.current?.getReveal()?.prev();
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='ctc-font slide-cont' style={{width: "600px", height: "600px"}}>
+      <div className="nav top-nav">
+        <button style={{background: "none", border: "none", flex: 0}} onClick={prevSlide}>
+          <PiCaretCircleLeftLight size={45} />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {currentSlide - 1 >= 0 && <div className='card-tag'>
+          <h4 className='roboto-medium'>{cardTitles[currentSlide - 1].toUpperCase()}</h4>
+        </div>}
+        <h4 className='roboto-medium' style={{transition: "all 0.7s ease-in-out"}}>{cardTitles[currentSlide].toUpperCase()}</h4>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <RevealSlides ref={slidesRef} plugins={[ReveaMarkdown]} progress={false} controls={false}  margin={0.225} scrollActivationWidth={0} onStateChange={handleOnStateChange} >
+        <section data-markdown="" data-background-color="#EBE8DE">
+          <script type="text/template">
+              {markdown}
+          </script>
+        </section>
+      </RevealSlides>
+      <div className="nav bottom-nav">
+        <button style={{background: "none", border: "none", flex: 0}} onClick={nextSlide} >
+          <PiCaretCircleRightLight size={45} />
+        </button>
+        <hr style={{borderColor: "black", flex: 1}}/>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default Cards
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Cards from './App.tsx'
 import './index.css'
+
+const sampleMarkdown = `# **Israel admits "mistake"** of killing civilians on bicycle 
+# **March 3, 2024**
+---
+**CONTEXT**
+
+A times analysis of drone footage published by the Israeli military on March 3 shows that the target of a strike was
+---
+## Slide 3`
+
+const sampleCardTitles = ["card 1", "card 2", "card 3", "card 4", "card 5"]
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Cards
+      markdown={sampleMarkdown}
+      cardTitles={sampleCardTitles}
+    />
   </React.StrictMode>,
 )
